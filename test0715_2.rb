c5f13b74a60d6421ecdfe4c8b41eb343ba05b92d
thinker_huang <huang@thinker-inc.jp>
2022-07-15 17:51:25 +0900

    ruby question for inherit

---
+++ test0715_2.rb
@@ -0,0 +1,28 @@
+class Huang
+    attr_accessor :name, :age
+
+    def initialize(name, age)
+        @name = name
+        @age = age
+    end
+
+    def show
+        puts "I am #{name}, at #{age}"
+    end
+end
+
+class TokyoHuang < Huang
+    attr_accessor :city
+
+    def sencond(city)
+        @city = city
+    end
+
+    def work
+        puts "#{name} at #{age} work in #{city}"
+    end
+end
+
+th = TokyoHuang.new('Huang', 27, 'tokyo')
+th.show
+th.work
